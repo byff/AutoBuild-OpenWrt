f2b381293830e0d36b3f1dbc68a99e25b837df63
byff <535937737@qq.com>
2022-05-02 13:53:06 +0800

    Update diy-part.sh

--- build/Lede_source/diy-part.sh
+++ build/Lede_source/diy-part.sh
@@ -5,3 +5,4 @@
 wget https://raw.githubusercontent.com/gd0772/patch/main/gd772.sh
 bash gd772.sh
 sed -i 's/02b79d5e2b07b5e64cd28f1fe84395ee11eef95fc49fd923a9ab93022b148be6/skip/g' feeds/packages/utils/containerd/Makefile
+curl -fsSL https://raw.githubusercontent.com/byff/AutoBuild-OpenWrt/main/build/Lede_source/diy/x86_index.htm > ./package/lean/autocore/files/x86/index.htm
